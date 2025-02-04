8702f362a27ea4f3c4ae41cf17b060f088d76653
seveibar <seveibar@gmail.com>
2025-02-04 15:40:04 -0500

    renaming and types for intranode solver

--- lib/types/high-density-types.ts
+++ lib/types/high-density-types.ts
@@ -6,3 +6,25 @@ export type NodeWithPortPoints = {
     y: number
   }[]
 }
+
+/**
+ * A path for a wire in high-density intra-node routing.
+ *
+ * Wires travel along a route, and are placed to avoid other
+ * wires at the same z-level. Any time a z level is changed,
+ * you must place a via.
+ *
+ * z is an integer corresponding to the layer index
+ *
+ * z=0: top layer for 2 layer boards
+ * z=1: bottom layer for 2 layer boards
+ *
+ * z must be an integer
+ */
+export type HighDensityIntraNodeRoute = {
+  connectionName: string
+  traceThickness: number
+  viaDiameter: number
+  route: Array<{ x: number; y: number; z: number }>
+  vias: Array<{ x: number; y: number }>
+}
