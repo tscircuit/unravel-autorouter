399d294e61ee3739d2887f9741315c5cc50e3cfa
seveibar <seveibar@gmail.com>
2025-02-22 00:34:22 -0800

    add second keyboard and a simple failing crossing case

---
+++ examples/keyboard2.fixture.tsx
@@ -0,0 +1,84 @@
+import { InteractiveGraphics } from "graphics-debug/react"
+import gkSample191 from "./assets/growing-grid-keyboard-sample-sample191-unrouted_simple_route.json"
+import { CapacityMeshSolver } from "lib/solvers/CapacityMeshSolver/CapacityMeshSolver"
+import type { SimpleRouteJson } from "lib/types"
+import { useState } from "react"
+import { combineVisualizations } from "lib/utils/combineVisualizations"
+
+export default () => {
+  const [solver] = useState(
+    () =>
+      new CapacityMeshSolver(gkSample191 as unknown as SimpleRouteJson, {
+        capacityDepth: 7,
+      }),
+  )
+  const [, forceUpdate] = useState({})
+  const failedHdSolvers = solver.highDensityRouteSolver?.failedSolvers
+
+  const animateToNextSolver = () => {
+    const currentSolver = solver.activeSolver
+    const interval = setInterval(() => {
+      solver.step()
+      forceUpdate({})
+      if (!solver.activeSolver || solver.activeSolver !== currentSolver) {
+        clearInterval(interval)
+      }
+    }, 10)
+  }
+
+  const animateUntilSolved = () => {
+    const interval = setInterval(() => {
+      for (let i = 0; i < 20; i++) {
+        solver.step()
+      }
+      forceUpdate({})
+      if (solver.solved || solver.failed) {
+        clearInterval(interval)
+      }
+    }, 10)
+  }
+
+  return (
+    <>
+      <div className="border p-1 m-1">Iterations {solver.iterations}</div>
+      <button
+        className="border m-2 p-2"
+        onClick={() => {
+          solver.step()
+          forceUpdate({})
+        }}
+      >
+        Step
+      </button>
+      <button className="border m-2 p-2" onClick={animateToNextSolver}>
+        Animate to next solver
+      </button>
+      <button className="border m-2 p-2" onClick={animateUntilSolved}>
+        Animate until solved
+      </button>
+      <InteractiveGraphics
+        graphics={
+          solver.solved || solver.failed
+            ? solver.visualize()
+            : // Fixes dumb animation bug
+              combineVisualizations({}, solver.visualize())
+        }
+      />
+      {failedHdSolvers?.map((s) => (
+        <button
+          className="border m-2 p-2"
+          key={s.nodeWithPortPoints.capacityMeshNodeId}
+          onClick={() => {
+            const json = JSON.stringify({
+              nodeWithPortPoints: s.nodeWithPortPoints,
+            })
+            navigator.clipboard.writeText(json)
+            alert("Copied to clipboard")
+          }}
+        >
+          {s.nodeWithPortPoints.capacityMeshNodeId}
+        </button>
+      ))}
+    </>
+  )
+}
