4ac648f0ee0003e717e6abf075e6a8673e76d9e9
seveibar <seveibar@gmail.com>
2025-02-25 11:50:11 -0800

    fix issue where targets outside obstacles don't work

---
+++ examples/e2e1.fixture.tsx
@@ -0,0 +1,37 @@
+import { InteractiveGraphics } from "graphics-debug/react"
+import { CapacityMeshSolver } from "lib/solvers/CapacityMeshSolver/CapacityMeshSolver"
+import { SimpleRouteJson } from "lib/types"
+
+const simpleRouteJson: SimpleRouteJson = {
+  layerCount: 2,
+  minTraceWidth: 0.2,
+  obstacles: [
+    // Create a rectangular obstacle in the center
+    {
+      type: "rect",
+      layers: ["top", "bottom"],
+      center: { x: 0, y: 0 },
+      width: 5,
+      height: 5,
+      connectedTo: [],
+    },
+  ],
+  connections: [
+    // Create a connection that needs to go around the obstacle
+    {
+      name: "conn1",
+      pointsToConnect: [
+        { x: -10, y: 5, layer: "top" },
+        { x: 10, y: 5, layer: "top" },
+      ],
+    },
+  ],
+  bounds: { minX: -15, maxX: 15, minY: -15, maxY: 15 },
+}
+
+export default () => {
+  const solver = new CapacityMeshSolver(simpleRouteJson)
+  solver.solve()
+
+  return <InteractiveGraphics graphics={solver.visualize()} />
+}
