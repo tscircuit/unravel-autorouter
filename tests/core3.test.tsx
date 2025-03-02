79c48fc316acc8bddcf2f45ae214a6a840a4b89a
seveibar <seveibar@gmail.com>
2025-03-01 23:02:59 -0800

    add example with core bug routing

---
+++ tests/core3.test.tsx
@@ -0,0 +1,50 @@
+import { RootCircuit, sel } from "@tscircuit/core"
+import { test, expect } from "bun:test"
+import { CapacityMeshAutorouterCoreBinding } from "./fixtures/CapacityMeshAutorouterCoreBinding"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { Fragment } from "react/jsx-runtime"
+
+test("core3 - 0402 columns", async () => {
+  const circuit = new RootCircuit()
+
+  circuit.add(
+    <board
+      width="10mm"
+      height="100mm"
+      autorouter={{
+        local: true,
+        groupMode: "subcircuit",
+      }}
+    >
+      {Array.from({ length: 30 }).map((_, i) => (
+        <Fragment key={i.toString()}>
+          <capacitor
+            capacitance="1000pF"
+            footprint="0402"
+            name={`C${i}`}
+            schX={-3}
+            pcbX={-3}
+            pcbY={(i / 30 - 0.5) * 60}
+          />
+          <resistor
+            resistance="1k"
+            footprint="0402"
+            name={`R${i}`}
+            schX={3}
+            pcbX={3}
+            pcbY={(i / 30 - 0.5) * 60}
+          />
+          <trace from={`.R${i} > .pin1`} to={`.C${i} > .pin1`} />
+        </Fragment>
+      ))}
+    </board>,
+  )
+
+  await circuit.renderUntilSettled()
+
+  const circuitJson = circuit.getCircuitJson()
+
+  expect(convertCircuitJsonToPcbSvg(circuitJson)).toMatchSvgSnapshot(
+    import.meta.path,
+  )
+})
