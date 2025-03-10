a44cfa5f61bd010fb14585ad73d17130a7776d04
seveibar <seveibar@gmail.com>
2025-03-10 16:05:49 -0700

    remove console log

--- lib/solvers/HighDensitySolver/IntraNodeSolver.ts
+++ lib/solvers/HighDensitySolver/IntraNodeSolver.ts
@@ -65,7 +65,6 @@ export class IntraNodeRouteSolver extends BaseSolver {
         { x, y, z: z ?? 0 },
       ])
     }
-    console.log({ unsolvedConnectionsMap })
     this.unsolvedConnections = Array.from(
       unsolvedConnectionsMap.entries().map(([connectionName, points]) => ({
         connectionName,
