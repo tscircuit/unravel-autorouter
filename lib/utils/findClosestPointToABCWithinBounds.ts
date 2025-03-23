16ba23b7f15654750fb2b5b5b81e59932c158d40
seveibar <seveibar@gmail.com>
2025-03-22 18:52:34 -0700

    fix type

--- lib/utils/findClosestPointToABCWithinBounds.ts
+++ lib/utils/findClosestPointToABCWithinBounds.ts
@@ -21,7 +21,7 @@ export function findClosestPointToABCWithinBounds(
   }
 
   // Function to calculate distance between two points
-  const distance = (p1, p2) => {
+  const distance = (p1: Point, p2: Point) => {
     return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
   }
 
