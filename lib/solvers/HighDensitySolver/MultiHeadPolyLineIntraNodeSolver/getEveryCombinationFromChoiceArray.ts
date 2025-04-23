a782cdd377b2baf0397edbf76165591572a39688
seveibar <seveibar@gmail.com>
2025-04-22 21:07:22 -0700

    setup choice array fn

---
+++ lib/solvers/HighDensitySolver/MultiHeadPolyLineIntraNodeSolver/getEveryCombinationFromChoiceArray.ts
@@ -0,0 +1,18 @@
+/**
+ * Get every combination from a choice array
+ *
+ * getEveryCombinationFromChoiceArray([[1, 2], [3, 4], [5, 6]])
+ *
+ * Returns:
+ * [
+ *  [1, 3, 5],
+ *  [1, 3, 6],
+ *  [1, 4, 5],
+ *  [1, 4, 6],
+ *  [2, 3, 5],
+ *  [2, 3, 6],
+ *  [2, 4, 5],
+ */
+export const getEveryCombinationFromChoiceArray = <T>(choiceArray: T[][]) => {
+  // TODO
+}
