0b8635b38a43425a3b9ac0ffa9420254aaf2080e
seveibar <seveibar@gmail.com>
2025-04-30 12:54:12 -0700

    wip bootstrap new setup for via possibilities solver integration

---
+++ lib/solvers/HighDensitySolver/MultiHeadPolyLineIntraNodeSolver/MultiHeadPolyLineIntraNodeSolver3_ViaPossibilitiesSolverIntegration.ts
@@ -0,0 +1,7 @@
+import { MultiHeadPolyLineIntraNodeSolver } from "./MultiHeadPolyLineIntraNodeSolver"
+
+export class MultiHeadPolyLineIntraNodeSolver3 extends MultiHeadPolyLineIntraNodeSolver {
+  setupInitialPolyLines(): void {
+    // TODO
+  }
+}
