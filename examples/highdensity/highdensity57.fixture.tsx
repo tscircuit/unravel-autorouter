243c8bc61a23aa06075f02b23b2d78700b32a041
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-06-24 17:53:27 +0000

    added fixtures for failed nodes

---
+++ examples/highdensity/highdensity57.fixture.tsx
@@ -0,0 +1,12 @@
+import cn4075 from "examples/assets/cn4075-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn4075.nodeWithPortPoints} />
+  )
+}
---
+++ examples/highdensity/highdensity58.fixture.tsx
@@ -0,0 +1,12 @@
+import cn2775 from "examples/assets/cn2775-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn2775.nodeWithPortPoints} />
+  )
+}
