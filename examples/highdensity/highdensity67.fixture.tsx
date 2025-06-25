24acf538216d9406cb67a7c6ed43f4705b159141
Ansh Grover <168731971+Anshgrover23@users.noreply.github.com>
2025-06-25 21:49:55 +0530

    add new fixtures for hyperdensity solver bugs (#165)
    * add new fixtures for hyperdensity solver bugs
    
    * rmv typecheck files

---
+++ examples/highdensity/highdensity67.fixture.tsx
@@ -0,0 +1,12 @@
+import cn3496 from "examples/assets/cn3496-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn3496.nodeWithPortPoints} />
+  )
+}
---
+++ examples/highdensity/highdensity69.fixture.tsx
@@ -0,0 +1,12 @@
+import cn1453 from "examples/assets/cn1453-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn1453.nodeWithPortPoints} />
+  )
+}
