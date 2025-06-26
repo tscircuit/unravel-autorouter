754fa6d311a83765851a37e2da53a611f1686ab9
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-06-26 23:17:27 +0530

    Add high density route solver fixtures for cn2776 and cn1472

---
+++ examples/highdensity/highdensity77.fixture.tsx
@@ -0,0 +1,12 @@
+import cn1472 from "examples/assets/cn1472-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn1472.nodeWithPortPoints} />
+  )
+}
