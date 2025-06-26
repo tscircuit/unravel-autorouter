6f68fc2c8841b4dd4fcbde74b1a66fd189adbbd1
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-06-26 23:22:27 +0530

    Replace cn1472 fixture with cn1722

--- examples/highdensity/highdensity77.fixture.tsx
+++ examples/highdensity/highdensity77.fixture.tsx
@@ -1,4 +1,4 @@
-import cn1472 from "examples/assets/cn1472-nodeWithPortPoints.json"
+import cn1722 from "examples/assets/cn1722-nodeWithPortPoints.json"
 import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
 
 export const hyperParameters = {
@@ -7,6 +7,6 @@ export const hyperParameters = {
 
 export default () => {
   return (
-    <HyperHighDensityDebugger nodeWithPortPoints={cn1472.nodeWithPortPoints} />
+    <HyperHighDensityDebugger nodeWithPortPoints={cn1722.nodeWithPortPoints} />
   )
 }
