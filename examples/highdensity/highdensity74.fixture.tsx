232d4a61a6d3e6ae92f2eca8eba901e2f8bd4856
Ansh Grover <168731971+Anshgrover23@users.noreply.github.com>
2025-06-26 22:06:38 +0530

    add more fixtures for autorouting hypersolver bugs (#172)

---
+++ examples/highdensity/highdensity74.fixture.tsx
@@ -0,0 +1,12 @@
+import cn1935 from "examples/assets/cn1935-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn1935.nodeWithPortPoints} />
+  )
+}
