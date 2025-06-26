184e0fcb4af74f7a4fc74b0cb6d6cb6a281f14fe
techmannih <manishchaudharyttt@gmail.com>
2025-06-26 22:33:32 +0530

    Add fixture for cn1936, cn2576 highDensityRouteSolver

---
+++ examples/highdensity/highdensity77.fixture.tsx
@@ -0,0 +1,12 @@
+import cn2576 from "examples/assets/cn2576-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn2576.nodeWithPortPoints} />
+  )
+}
