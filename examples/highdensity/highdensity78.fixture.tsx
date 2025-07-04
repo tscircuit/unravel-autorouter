735643b723040534a5c812b71001b949705f3f3e
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2025-07-04 23:21:41 +0200

    added hydensity cn673 reproduction

---
+++ examples/highdensity/highdensity78.fixture.tsx
@@ -0,0 +1,12 @@
+import cn673 from "examples/assets/cn673-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn673.nodeWithPortPoints} />
+  )
+}
