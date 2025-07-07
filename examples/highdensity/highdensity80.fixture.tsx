bd49af0689c36e1b054239affa11c70133c17266
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-07-07 14:26:08 +0000

    Added fixtures for highdensity

---
+++ examples/highdensity/highdensity80.fixture.tsx
@@ -0,0 +1,12 @@
+import cn1103 from "examples/assets/cn1103-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn1103.nodeWithPortPoints} />
+  )
+}
---
+++ examples/highdensity/highdensity85.fixture.tsx
@@ -0,0 +1,12 @@
+import cn3988 from "examples/assets/cn3988-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export const hyperParameters = {
+  SEGMENTS_PER_POLYLINE: 6,
+}
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn3988.nodeWithPortPoints} />
+  )
+}
