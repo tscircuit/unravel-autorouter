72174da1d244efc83129bf95dcfcf8629afe8f2e
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2025-07-14 10:35:27 +0200

    added hyperdensity fixture for node cn785

---
+++ examples/highdensity/highdensity82.fixture.tsx
@@ -0,0 +1,8 @@
+import cn785 from "examples/assets/cn785-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn785.nodeWithPortPoints} />
+  )
+}
