c4d40175b45001635c02b74277fb322cf0bf5349
Shibo <shibosoftwaredev@gmail.com>
2025-07-14 12:30:30 +0200

    added hyperdensity fixture for node 1317 (#207)
    * added hyperdensity fixture for node 1317
    
    * rename file

---
+++ examples/highdensity/highdensity84.fixture.tsx
@@ -0,0 +1,8 @@
+import cn1317 from "examples/assets/cn1317-nodeWithPortPoints.json"
+import { HyperHighDensityDebugger } from "lib/testing/HyperHighDensityDebugger"
+
+export default () => {
+  return (
+    <HyperHighDensityDebugger nodeWithPortPoints={cn1317.nodeWithPortPoints} />
+  )
+}
